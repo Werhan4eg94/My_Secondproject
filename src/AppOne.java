2b78e289164492bd9a05128ed5aabc58335a5224
Werhan4eg94 <Werhan4e41111@gmail.com>
2021-12-22 13:28:41 +0200

    changes default message

--- src/AppOne.java
+++ src/AppOne.java
@@ -3,7 +3,7 @@ public class AppOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.print("hello");
+			System.out.print("hello Andrew new changes");
 	}
 
 }
